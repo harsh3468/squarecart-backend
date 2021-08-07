4a2605902387bb80c5aa0c585c1049be1de8b0a5
harsh3468 <harshkumargoel.3468@gmail.com>
2021-08-07 18:41:39 +0530

    first commit

--- router/orders.js
+++ router/orders.js
@@ -1,23 +1,23 @@
-const {dbconnect} = require('../config')
+const { dbconnect } = require('../config')
 
-exports.generateOrder = async(req,res)=>{
+exports.generateOrder = async (req, res) => {
     await dbconnect.query('SET FOREIGN_KEY_CHECKS=0');
-    dbconnect.query('INSERT INTO order_detail (user_id,total_amount,user_address) VALUES (?,?,?)',[req.body.user_id,req.body.total_amount,req.body.user_address],async (error,result,fields)=>{
+    dbconnect.query('INSERT INTO order_detail (user_id,total_amount,user_address,order_date) VALUES (?,?,?,?)', [req.body.user_id, req.body.total_amount,req.body.user_address,new Date()], async (error, result, fields) => {
         await dbconnect.query('SET FOREIGN_KEY_CHECKS=1');
-        if(error){
-            return res.status(400).json(error.sqlMessage);;
+        if (error) {
+            return res.status(400).json(error.sqlMessage);
         }
-        return res.status(201).json(result.insertId);
+        orderedProduct(req.body.orderMap, result.insertId, () => {
+            return res.status(201).json(result.insertId)
+        })
     })
 }
 
-exports.orderedProduct = async(req,res)=>{
+const orderedProduct = async (orderMap, orderId, callback) => {
     await dbconnect.query('SET FOREIGN_KEY_CHECKS=0');
-    dbconnect.query('INSERT INTO order_detail (order_id,order,user_address) VALUES (?,?,?)',[req.body.user_id,req.body.total_amount,req.body.user_address],async (error,result,fields)=>{
-        await dbconnect.query('SET FOREIGN_KEY_CHECKS=1');
-        if(error){
-            return res.status(400).json(error.sqlMessage);;
-        }
-        return res.status(201).json(result.insertId);
-    })
+    orderMap.forEach(data => {
+        dbconnect.query('INSERT INTO order_items (order_id,product_id,quantity) VALUES (?,?,?)', [orderId, data.id, data.quantity])
+    });
+    await dbconnect.query('SET FOREIGN_KEY_CHECKS=1');
+    callback()
 }
--- router/user.js
+++ router/user.js
@@ -5,10 +5,11 @@ const {dbconnect} = require('../config')
 exports.loginUser = (req,res)=>{
     dbconnect.query('SELECT id,password FROM users WHERE email=(?)',[req.body.email],(error,result,fields)=>{
     if(error){
+        console.log(error)
         return res.status(400).json(error.sqlMessage);
     }
     if(result.length){
-        if(bcrypt.compareSync(req.body.password,result[0].password)){
+        if(!bcrypt.compareSync(req.body.password,result[0].password)){
             return res.status(400).json({error:"wrong password"});
         }else{
             return res.status(200).json({user_id:result[0].id,token:jwt.sign({data:'cartsquare'},'happy@cart')});
@@ -20,9 +21,11 @@ exports.loginUser = (req,res)=>{
 })
 }
 exports.signupUser = (req,res)=>{
-    dbconnect.query('INSERT INTO users (email,password) VALUES (?,?)',[req.body.email,bcrypt.hashSync(req.body.password,8)
+    console.log(req.body)
+    dbconnect.query('INSERT INTO users (email,first_name,last_name,phone_number,password) VALUES (?,?,?,?,?)',[req.body.email,req.body.firstName,req.body.lastName,req.body.phoneNumber,bcrypt.hashSync(req.body.password,8)
         ],(error,result,fields)=>{
         if(error){
+            console.log(error)
             return res.status(400).json(error.sqlMessage);
         }
         return res.status(201).json(result);
